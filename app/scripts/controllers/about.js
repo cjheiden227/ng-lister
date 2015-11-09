cc0d2440fbe82dc0ce092321ad1769c570a117fa
Chris Heiden <cjheiden227@gmail.com>
2015-11-09 07:57:19 -0800

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngListerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ngListerApp
+ */
+angular.module('ngListerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngListerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngListerApp
+ */
+angular.module('ngListerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
